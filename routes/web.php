f74db1f09b3ba9ffc3d609f6ce37740ce5498181
yuk-hrt <62339023+yuk-hrt@users.noreply.github.com>
2020-10-21 14:56:41 +0900

    viewとコントローラ

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,6 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
