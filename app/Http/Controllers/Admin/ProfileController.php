a6a8443db0cbd2e25270c3767f040ccb7c6af49d
yuk-hrt <62339023+yuk-hrt@users.noreply.github.com>
2020-10-23 18:43:43 +0900

    投稿したニュース一覧を表示しよう

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -8,6 +8,19 @@ use App\Profile;
 
 class ProfileController extends Controller
 {
+    public function index(Request $request)
+    {
+        $cond_title = $request->cond_title;
+        if ($cond_title != '') {
+            // 検索されたら検索結果を取得する
+            $posts = Profile::where('name', $cond_title)->get();
+        } else {
+            // それ以外はすべてのニュースを取得する
+            $posts = Profile::all();
+        }
+        return view('admin.profile.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+    }
+
     public function add()
     {
         return view('admin.profile.create');
--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,11 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+    Route::get('news', 'Admin\NewsController@index');
     Route::get('news/create', 'Admin\NewsController@add');
     Route::post('news/create', 'Admin\NewsController@create');
 
+    Route::get('profile', 'Admin\ProfileController@index');
     Route::get('profile/create', 'Admin\ProfileController@add');
     Route::post('profile/create', 'Admin\ProfileController@create');
     Route::get('profile/edit', 'Admin\ProfileController@edit');
